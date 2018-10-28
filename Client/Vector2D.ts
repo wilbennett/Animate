9d04c038c83eb76540d5ed644fc73e2578053ad4
Wil <wilbennettjr@live.com>
2018-10-28 11:43:41 -0400

    Added normal. Added reflection methods.

--- Client/Vector2D.ts
+++ Client/Vector2D.ts
@@ -4,6 +4,7 @@
     private _degrees: number = -1;
     private _radians: number = -1;
     private _polar: Polar | null = null;
+    private _normal: Vector2D | null = null;
 
     constructor(private _x: number, private _y: number) {
     }
@@ -57,26 +58,38 @@
         return this._polar;
     }
 
+    get normal() {
+        if (!this._normal) {
+            this._normal = new Vector2D(this.y, -this.x);
+        }
+
+        return this._normal;
+    }
+
     toString() { "(" + this._x.toFixed(3) + ", " + this._y.toFixed(3) + ")"; }
 
     static get empty() { return new Vector2D(0, 0); }
 
     clone(): Vector2D {
         let result = new Vector2D(this.x, this.y);
+        result._magSquared = this._magSquared;
         result._mag = this._mag;
         result._degrees = this._degrees;
         result._radians = this._radians;
         result._polar = this._polar;
+        result._normal = this._normal;
         return result;
     }
 
     toPoint(): Point2D { return new Point2D(this.x, this.y); }
 
     reset() {
+        this._magSquared = -1;
         this._mag = -1;
         this._degrees = -1;
         this._radians = -1;
         this._polar = null;
+        this._normal = null;
     }
 
     add(other: Vector2D): Vector2D {
@@ -107,11 +120,8 @@
         return this;
     }
 
-    dot(scale: number): Vector2D {
-        this._x /= scale;
-        this._y /= scale;
-        this.reset();
-        return this;
+    dot(other: Vector2D): number {
+        return Math2D.dot(this.x, this.y, other.x, other.y);
     }
 
     normalize(): Vector2D {
@@ -124,9 +134,37 @@
         return this;
     }
 
+    reflectViaNormal(normal: Vector2D): Vector2D {
+        // -(2 * (v . normal) * normal - v)
+        let dot2 = 2 * this.dot(normal);
+        let dot2TimesNormal = Vector2D.mult(dot2, normal);
+        return dot2TimesNormal.subtract(this).mult(-1);
+
+        // v - 2 * (v . normal) * normal
+        //return this.subtract(dot2TimesNormal);
+    }
+
+    reflectOff(reflector: Vector2D): Vector2D {
+        return this.reflectViaNormal(reflector.normal);
+    }
+
+    reflect(source: Vector2D): Vector2D {
+        return source.reflectViaNormal(this.normal);
+    }
+
+    draw(ctx: CanvasRenderingContext2D, size: number, color: string) {
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, size, 0, MathEx.TWO_PI, true);
+        ctx.fillStyle = color;
+        ctx.fill();
+        ctx.strokeStyle = color;
+        ctx.stroke();
+    }
+
     static add(v1: Vector2D, v2: Vector2D): Vector2D { return v1.clone().add(v2); }
     static subtract(v1: Vector2D, v2: Vector2D): Vector2D { return v1.clone().subtract(v2); }
     static normalize(v: Vector2D): Vector2D { return v.clone().normalize(); }
+    static dot(v1: Vector2D, v2: Vector2D): number { return v1.dot(v2); }
 
     static mult(v: Vector2D, scale: number): Vector2D;
     static mult(scale: number, v: Vector2D): Vector2D;
--- wwwroot/scripts/Vector2D.js
+++ wwwroot/scripts/Vector2D.js
@@ -8,6 +8,7 @@ var Vector2D = /** @class */ (function () {
         this._degrees = -1;
         this._radians = -1;
         this._polar = null;
+        this._normal = null;
     }
     Object.defineProperty(Vector2D.prototype, "x", {
         get: function () { return this._x; },
@@ -72,6 +73,16 @@ var Vector2D = /** @class */ (function () {
         enumerable: true,
         configurable: true
     });
+    Object.defineProperty(Vector2D.prototype, "normal", {
+        get: function () {
+            if (!this._normal) {
+                this._normal = new Vector2D(this.y, -this.x);
+            }
+            return this._normal;
+        },
+        enumerable: true,
+        configurable: true
+    });
     Vector2D.prototype.toString = function () { "(" + this._x.toFixed(3) + ", " + this._y.toFixed(3) + ")"; };
     Object.defineProperty(Vector2D, "empty", {
         get: function () { return new Vector2D(0, 0); },
@@ -80,18 +91,22 @@ var Vector2D = /** @class */ (function () {
     });
     Vector2D.prototype.clone = function () {
         var result = new Vector2D(this.x, this.y);
+        result._magSquared = this._magSquared;
         result._mag = this._mag;
         result._degrees = this._degrees;
         result._radians = this._radians;
         result._polar = this._polar;
+        result._normal = this._normal;
         return result;
     };
     Vector2D.prototype.toPoint = function () { return new Point2D(this.x, this.y); };
     Vector2D.prototype.reset = function () {
+        this._magSquared = -1;
         this._mag = -1;
         this._degrees = -1;
         this._radians = -1;
         this._polar = null;
+        this._normal = null;
     };
     Vector2D.prototype.add = function (other) {
         this._x += other.x;
@@ -117,11 +132,8 @@ var Vector2D = /** @class */ (function () {
         this.reset();
         return this;
     };
-    Vector2D.prototype.dot = function (scale) {
-        this._x /= scale;
-        this._y /= scale;
-        this.reset();
-        return this;
+    Vector2D.prototype.dot = function (other) {
+        return Math2D.dot(this.x, this.y, other.x, other.y);
     };
     Vector2D.prototype.normalize = function () {
         var m = this.mag;
@@ -131,9 +143,32 @@ var Vector2D = /** @class */ (function () {
         this.reset();
         return this;
     };
+    Vector2D.prototype.reflectViaNormal = function (normal) {
+        // -(2 * (v . normal) * normal - v)
+        var dot2 = 2 * this.dot(normal);
+        var dot2TimesNormal = Vector2D.mult(dot2, normal);
+        return dot2TimesNormal.subtract(this).mult(-1);
+        // v - 2 * (v . normal) * normal
+        //return this.subtract(dot2TimesNormal);
+    };
+    Vector2D.prototype.reflectOff = function (reflector) {
+        return this.reflectViaNormal(reflector.normal);
+    };
+    Vector2D.prototype.reflect = function (source) {
+        return source.reflectViaNormal(this.normal);
+    };
+    Vector2D.prototype.draw = function (ctx, size, color) {
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, size, 0, MathEx.TWO_PI, true);
+        ctx.fillStyle = color;
+        ctx.fill();
+        ctx.strokeStyle = color;
+        ctx.stroke();
+    };
     Vector2D.add = function (v1, v2) { return v1.clone().add(v2); };
     Vector2D.subtract = function (v1, v2) { return v1.clone().subtract(v2); };
     Vector2D.normalize = function (v) { return v.clone().normalize(); };
+    Vector2D.dot = function (v1, v2) { return v1.dot(v2); };
     Vector2D.mult = function (scale, v) {
         var vec;
         var scalar;
